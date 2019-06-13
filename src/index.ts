cdbd488354105fbc6a38d049ac558e27f0b0a896
zhikiri <zhikiri.dev@gmail.com>
2019-06-13 14:27:49 +0300

    added basic mimic logic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response } from 'express';
+import { Mimic } from './mimic';
+
 const app = express();
+const mimic = new Mimic(`/app/mocks`);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World');
-});
+(async () => {
 
-app.listen(8080, () => {
-  console.log('Server started and listening :8080');
-});
+  const routes = await mimic.getMockedRoutes();
+  routes.map(({ method, path, handler }) => {
+
+    app[method](path, async (req: Request, res: Response) => {
+
+      const mock = await handler();
+      res.json(JSON.parse(mock.toString()));
+    });
+
+    console.log(`Route ${method.toUpperCase()} '${path}' was set`);
+  });
+
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World');
+  });
+
+  app.listen(8080, () => {
+    console.log('Server started and listening :8080');
+  });
+})();
---
+++ src/mimic.ts
@@ -0,0 +1,60 @@
+import fs from 'fs';
+import path from 'path';
+
+type RouteHandler = () => Promise<Buffer>;
+type RouteMethod = 'get' | 'post' | 'put' | 'delete';
+
+export interface Route {
+  method: RouteMethod;
+  path: string;
+  handler: RouteHandler;
+}
+
+export class Mimic {
+
+  public constructor(private mockPath: string) { }
+
+  public async getMockedRoutes(): Promise<Route[]> {
+
+    const files = await this.getMockFiles();
+    return files.map((filePath: string) => this.getRoute(filePath));
+  }
+
+  private getRoute(filePath: string): Route {
+
+    const source = filePath.split('.');
+    source.pop();
+
+    return {
+      method: source.shift() as RouteMethod,
+      path: `/${source.join('/')}`,
+      handler: this.getRouteHandler(path.join(this.mockPath, filePath))
+    };
+  }
+
+  private getRouteHandler(filePath: string): RouteHandler {
+
+    return () => {
+
+      return new Promise((resolve, reject) => {
+
+        fs.readFile(filePath, (err: Error | null, data: Buffer) => {
+
+          return err != null ? reject(err) : resolve(data);
+        });
+      });
+    }
+  }
+
+  private getMockFiles(): Promise<string[]> {
+
+    return new Promise((resolve, reject) => {
+
+      fs.readdir(this.mockPath, (err: Error | null, files: string[]) => {
+
+        const filtered = files.filter((file: string) => file != '.gitkeep');
+        return err != null ? reject(err) : resolve(filtered);
+      });
+    });
+  }
+}
