e61eb3cb4853a70d3fa044c831877797de54ce8d
zhikiri <zhikiri.dev@gmail.com>
2019-06-14 15:57:51 +0300

    refactored index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 import config from './config';
 import { Mimic, Route } from './lib/mimic';
 import Cache from './lib/cache';
+import Api from './lib/api';
 
 const app = express();
 const mimic = new Mimic(config.MOCKS_PATH, new Cache());
@@ -10,20 +11,12 @@ const mimic = new Mimic(config.MOCKS_PATH, new Cache());
 (async () => {
 
   const routes = await mimic.getMockedRoutes();
-  routes.map((route: Route) => {
+  const api = new Api(routes);
 
-    app[route.method](route.path, async (req: Request, res: Response) => {
+  api.setMockRoutes(app);
 
-      const mock = await route.handler();
-      res.json(JSON.parse(mock));
-    });
-
-    console.log(`Route ${route.method.toUpperCase()} '${route.path}' was set`);
-  });
-
-  app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World');
-  });
+  app.get('/mimic/routes/', api.getRoutes);
+  app.get('/mimic/routes/:id', api.getRouteById);
 
   app.listen(config.API_PORT, () => {
     console.log(`Server started and listening :${config.API_PORT}`);
