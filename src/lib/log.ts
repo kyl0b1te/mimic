d85fe9da4fe604dcc042197b229c3efb10f24a0b
zhikiri <zhikiri.dev@gmail.com>
2019-06-22 13:26:03 +0300

    added timestamp as a log record parameter

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -4,6 +4,7 @@ export interface LogRecord {
   headers: IncomingHttpHeaders;
   query: Record<string, any>[];
   body: Record<string, any>[];
+  timestamp?: number;
 }
 
 export default class Log {
@@ -15,11 +16,16 @@ export default class Log {
     if (!Log.logs[hash]) {
       Log.logs[hash] = [];
     }
-    Log.logs[hash].push(record);
+    Log.logs[hash].push({ timestamp: Log.getTimestamp(), ...record });
   }
 
   public static getLogs(hash: string): LogRecord[] {
 
     return Log.logs[hash] || [];
+  }
+
+  private static getTimestamp(): number {
+
+    return (new Date()).getTime();
   }
 }
