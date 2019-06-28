53a915dd8ff729f349e4e2260ebb17d4866a1022
zhikiri <zhikiri.dev@gmail.com>
2019-06-28 13:17:19 +0300

    removed log class old logic

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -30,28 +30,3 @@ export default class Log {
     return this.cache.get(`logs.${hash}`) || [];
   }
 }
-
-/*
-export default class Log {
-
-  private static logs: { [hash: string]: LogRecord[] } = {};
-
-  public static save(hash: string, record: LogRecord): void {
-
-    if (!Log.logs[hash]) {
-      Log.logs[hash] = [];
-    }
-    Log.logs[hash].push({ timestamp: Log.getTimestamp(), ...record });
-  }
-
-  public static getLogs(hash: string): LogRecord[] {
-
-    return Log.logs[hash] || [];
-  }
-
-  private static getTimestamp(): number {
-
-    return (new Date()).getTime();
-  }
-}
-*/
